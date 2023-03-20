67b7f9e573e5490f1d573b7f3e2cf171f41a38c9
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-03-20 11:39:02 +0900

    init

---
+++ src/test/java/com/qa/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
