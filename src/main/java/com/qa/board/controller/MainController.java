03055c335215b5c709896295bcffc69052bcacf5
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-03-20 15:52:17 +0900

    feat: Controller 생성 후 동작 확인 테스트
    More Details:
    - 컨트롤러 정상 작동 확인을 위한 junit5 컨트롤러 테스트.

---
+++ src/main/java/com/qa/board/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.qa.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/home")
+    @ResponseBody
+    public String home() {
+        return "home request";
+    }
+
+
+
+}
---
+++ src/test/java/com/qa/board/controller/MainControllerTest.java
@@ -0,0 +1,41 @@
+package com.qa.board.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MainControllerTest {
+
+    @Autowired
+    private MainController mainController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("/home 요청에 home request 반환")
+    void test1() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/home")
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string("home request"))
+                .andDo(print());
+    }
+
+
+}
