0ae2498be4ed732c2d4ac4f4b72506630995a9a4
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-03-22 16:23:13 +0900

    feat: 회원가입 기능 추가
    More Details:
    - 회원가입 폼에서 오류 발생 시 예외처리.
    - 중복 이메일, 중복 아이디 처리.

---
+++ src/main/java/com/qa/board/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.qa.board.controller;
+
+import com.qa.board.form.UserCreateForm;
+import com.qa.board.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/signUp")
+    public String signUp(UserCreateForm userCreateForm) {
+        return "signUpForm";
+    }
+
+    @PostMapping("/signUp")
+    public String signUp(@Valid UserCreateForm userCreateForm,
+                         BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "signUpForm";
+        }
+        if (!userCreateForm.getPassword().equals(userCreateForm.getPasswordAgain())) {
+            bindingResult.rejectValue("passwordAgain",
+                    "IncorrectPassword", "두 비밀번호가 일치하지 않습니다.");
+            return "signUpForm";
+        }
+
+        try {
+            userService.createUser(userCreateForm);
+        } catch (DataIntegrityViolationException e) {
+            bindingResult.reject("signUpFailed", "이미 존재하는 회원입니다.");
+            return "signUpForm";
+        } catch (Exception e) {
+            bindingResult.reject("signUpFailed", e.getMessage());
+            return "signUpForm";
+        }
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/qa/board/service/UserService.java
@@ -0,0 +1,33 @@
+package com.qa.board.service;
+
+import com.qa.board.domain.SiteUser;
+import com.qa.board.form.UserCreateForm;
+import com.qa.board.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public SiteUser createUser(UserCreateForm userCreate) {
+        String encodedPassword = passwordEncoder.encode(userCreate.getPassword());
+
+        return userRepository.save(SiteUser.builder()
+                .username(userCreate.getUsername())
+                .password(encodedPassword)
+                .email(userCreate.getEmail())
+                .build());
+    }
+
+
+
+
+}
