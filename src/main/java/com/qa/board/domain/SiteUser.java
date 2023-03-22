580e60ad991471894d39b4c24c45aa16728844f2
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-03-22 16:21:53 +0900

    feat: SiteUser 엔티티 생성
    More Details:
    - 새로운 엔티티 SiteUser (사용자) 생성.

---
+++ src/main/java/com/qa/board/domain/SiteUser.java
@@ -0,0 +1,31 @@
+package com.qa.board.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+import static jakarta.persistence.GenerationType.*;
+
+@Entity
+@Getter
+public class SiteUser {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Builder
+    public SiteUser(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+}
---
+++ src/main/java/com/qa/board/form/UserCreateForm.java
@@ -0,0 +1,29 @@
+package com.qa.board.form;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserCreateForm {
+
+    @Size(min = 3, max = 25)
+    @NotEmpty(message = "아이디를 입력해주세요.")
+    private String username;
+
+    @NotEmpty(message = "비밀번호를 입력해주세요.")
+    private String password;
+
+    @NotEmpty(message = "비밀번호 확인을 입력해주세요.")
+    private String passwordAgain;
+
+    @NotEmpty(message = "이메일을 입력해주세요.")
+    @Email
+    private String email;
+
+
+
+}
