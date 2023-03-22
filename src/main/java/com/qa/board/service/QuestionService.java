2bba34b7136db44fe2cb3d82b367fda50e846611
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-03-22 11:32:27 +0900

    fix: 작성일자 기준 내림차순 정렬
    More Details:
    - 가장 최신의 글이 먼저 보여지도록 desc sort.

--- src/main/java/com/qa/board/service/QuestionService.java
+++ src/main/java/com/qa/board/service/QuestionService.java
@@ -6,8 +6,10 @@ import com.qa.board.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -17,7 +19,7 @@ public class QuestionService {
     private final QuestionRepository questionRepository;
 
     public Page<Question> findQuestions(int page) {
-        PageRequest pageable = PageRequest.of(page, 10);
+        PageRequest pageable = PageRequest.of(page, 10, Sort.by(Sort.Order.desc("createdDate")));
         return questionRepository.findAll(pageable);
     }
 
