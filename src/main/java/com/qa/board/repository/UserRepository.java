72edcae19bc407b0b76e2031da7281c2547c6808
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-03-23 14:41:51 +0900

    feat: 로그인 기능을 위한 컨트롤러, 레포지토리 메서드 추가.

--- src/main/java/com/qa/board/repository/UserRepository.java
+++ src/main/java/com/qa/board/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.qa.board.repository;
 import com.qa.board.domain.SiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByUsername(String username);
 }
