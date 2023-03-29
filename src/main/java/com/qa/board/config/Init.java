b47146ef264db3a687583275c6d10ed46d983e5b
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-03-29 10:43:07 +0900

    fix: 글 수정 버튼 동작 확인을 위한 데이터 추가
    - 글 작성자인지 여부 확인 테스트를 위한 데이터 추가.

--- src/main/java/com/qa/board/config/Init.java
+++ src/main/java/com/qa/board/config/Init.java
@@ -2,18 +2,26 @@ package com.qa.board.config;
 import com.qa.board.domain.Question;
 import com.qa.board.domain.SiteUser;
 import com.qa.board.repository.QuestionRepository;
+import com.qa.board.repository.UserRepository;
 import com.qa.board.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Profile({"dev", "test"})
 // Init : 개발환경이 이거나, 테스트환경일 때만 실행
 public class Init {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Bean
-    public CommandLineRunner initData(QuestionService questionService) {
+    public CommandLineRunner initData(QuestionService questionService,
+                                      UserRepository userRepository) {
         return args -> {
             // 이 부분은 스프링부트 앱이 실행되고, 본격적으로 작동하기 전에 실행된다.
             for (int i = 1; i <= 300; i++) {
@@ -21,6 +29,15 @@ public class Init {
                 String content = "[내용] - " + i;
                 questionService.createQuestion(title, content, null);
             }
+            String encodedPassword = passwordEncoder.encode("111");
+
+            SiteUser user = SiteUser.builder()
+                    .username("bin")
+                    .password(encodedPassword)
+                    .email("bot@gmail.com")
+                    .build();
+            userRepository.save(user);
+            questionService.createQuestion("bin-1", "bin-1", user);
         };
     }
 }
