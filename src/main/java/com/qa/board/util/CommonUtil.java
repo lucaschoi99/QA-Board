abad59849a6ea1b0a8400e3d9c6eca143a0a36c3
Minsoo Choi <lucas0209@yonsei.ac.kr>
2023-03-30 10:12:42 +0900

    feat: 글, 답변에 마크다운 문법 사용 가능

---
+++ src/main/java/com/qa/board/util/CommonUtil.java
@@ -0,0 +1,19 @@
+package com.qa.board.util;
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommonUtil {
+
+    // 마크다운 문법이 적용된 텍스트를 변환된 HTML로 리턴
+    public String markdown(String markdown) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdown);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+
+}
